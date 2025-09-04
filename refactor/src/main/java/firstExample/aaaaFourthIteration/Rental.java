7c4405a3fd0ce1427c93701de0ca7bfd0b17716e
SONIL KUMAR <sonil@SONILs-MacBook-Air.local>
2025-09-05 00:33:47 +0530

    restructuring folders

---
+++ refactor/src/main/java/firstExample/aaaaFourthIteration/Rental.java
@@ -0,0 +1,31 @@
+package src.main.java.firstExample.aaaaFourthIteration;
+
+class Rental {
+    private Movie _movie;
+    private int _daysRented;
+
+    public Rental(Movie movie, int daysRented) {
+        _movie = movie;
+        _daysRented = daysRented;
+    }
+
+    public int getDaysRented() {
+        return _daysRented;
+    }
+
+    public Movie getMovie() {
+        return _movie;
+    }
+
+    //// Moved Method from Customer
+    double getCharge() {
+        ////getCharge should move onto movie, as it is a bad idea to do a switch based on an attribute of another object.
+        return _movie.getCharge(_daysRented);
+    }
+
+    //// Moved Method from SecondIteration
+    int getFrequentRenterPoints() {
+        //// same as above moved to Movie, as it belongs there
+        return _movie.getFrequentRenterPoints(_daysRented);
+    }
+}
